ac4ef3800bbd39a50fffe69db31215c4bb22e30c
Liam <liam.cosford.03@gmail.com>
2026-09-02 12:43:40 +0200

    Changed the log command to save *EVERY* (valid) command the user inputs - Redo no longer functions because of this.
    Also added Log and Redo to the help command. So it now displays these commands too.

--- Program.cs
+++ Program.cs
@@ -18,12 +18,12 @@ string currentRoom = grid[(int)userPosition.Y, (int)userPosition.X]; // Current 
 int turnsLeft = grid.GetUpperBound(0) * grid.GetUpperBound(1); // turns left before the game ends
 bool isPlaying = true;
 
-List<Vector2> Log = new List<Vector2>(); // Log of the user's movements in the grid
-Log.Add(userPosition); // add the starting/origin position of the user
+List<string> Log = new List<string>(); // Log of the user's commands
 
-// Defining and setting where the key and the goal are located
+// Defining and setting where the special rooms are located in the grid
 Vector2 keyPosition = new Vector2(0, 0);  // Temporary position until we find the key in the grid
 Vector2 goalPosition = new Vector2(0, 0); // Temporary position until we find the goal in the grid
+Vector2 RPS_Position = new Vector2(0, 0);  // Temporary position until we find the RPS room in the grid
 for (int y = 0; y < grid.GetLength(0); y++)
 {
     for (int x = 0; x < grid.GetLength(1); x++)
@@ -35,6 +35,10 @@ for (int y = 0; y < grid.GetLength(0); y++)
         if (grid[y, x] == " G ")
         {
             goalPosition = new Vector2(x, y);
+        }
+        if (grid[y, x] == "RPS")
+        {
+            RPS_Position = new Vector2(x, y);
         }
     }
 }
@@ -58,6 +62,7 @@ Console.WriteLine("(X is your position)");
 
 // Display the help menu at the start of the game, so the user can see what commands are available to them
 commandSelector("help");
+Log.RemoveAt(Log.Count - 1);
 
 while (isPlaying && turnsLeft > 0)
 {
@@ -102,12 +107,14 @@ while (isPlaying && turnsLeft > 0)
         // Check if the user wants to use the key or not
         if (userInput == "yes")
         {
+            Log.Add(userInput); // Add the inputed command to the log
             Console.WriteLine("\nYou use the key to unlock the doors.\nAs you (obviously) open it, you see that beyond it lays...\n\n(Well we'll see. You'll have to stay tuned untill the next installment of ZORK\n\nYOU WIN!!!");
             isPlaying = false;
             break;
         }
         else if (userInput == "no")
         {
+            Log.Add(userInput); // Add the inputed command to the log
             Console.WriteLine("\nstrange... There's not much else you can do besides finding this, the key and picking it up. But alright...");
             Console.WriteLine("\nPress 'Enter' to continue");
             Console.ReadLine();
@@ -123,12 +130,15 @@ while (isPlaying && turnsLeft > 0)
             // Check if the user wants to use the key
             if (userInput == "yes")
             {
+                Log.Add(userInput); // Add the inputed command to the log
+
                 Console.WriteLine("\nYou use the key to unlock the heavy door and beyond it lays...\n(Well we'll see you'll have to stay tuned untill the next installment of ZORK\nYOU WIN!!!");
                 isPlaying = false;
                 break;
             }
             else if (userInput == "no")
             {
+                Log.Add(userInput); // Add the inputed command to the log
                 Console.WriteLine("\nstrange... There's not much else you can do besides this but alright...");
             }
         }
@@ -153,6 +163,8 @@ while (isPlaying && turnsLeft > 0)
         // Prompt the user to pick up the key or not, and handle their response
         if (userInput == "yes")
         {
+            Log.Add(userInput); // Add the inputed command to the log
+
             Console.WriteLine("\nYou pick up the key!");
             keyPosition = new Vector2(-1, -1); // just to make it dissappear
             userHasKey = true;
@@ -163,6 +175,8 @@ while (isPlaying && turnsLeft > 0)
         }
         else if (userInput == "no")
         {
+            Log.Add(userInput); // Add the inputed command to the log
+
             userHasFoundKey = true;
             Console.WriteLine("strange... You'll need the key to win the game, but I guess you can keep looking for the goal without it...\n");
             Console.WriteLine("Press 'Enter' to continue");
@@ -179,6 +193,8 @@ while (isPlaying && turnsLeft > 0)
             // Check (again) if the user wants to pick up the key
             if (userInput == "yes")
             {
+                Log.Add(userInput); // Add the inputed command to the log
+
                 Console.Clear();
                 Console.WriteLine("You pick up the key!");
                 keyPosition = new Vector2(-1, -1); // just to make the key none interactable
@@ -191,6 +207,8 @@ while (isPlaying && turnsLeft > 0)
             // or not
             else if (userInput == "no")
             {
+                Log.Add(userInput); // Add the inputed command to the log
+
                 userHasFoundKey = true;
                 Console.Clear();
                 Console.WriteLine("strange... You'll need the key to win the game, but I guess you can keep looking for the goal without it...\n");
@@ -289,7 +307,7 @@ void displayMap(Vector2 userPosition)
     Console.WriteLine("|");
 }
 
-// Function to handle user input for movement and certain other commands
+// Function to handle user input for movement and other commands (excluding RPS minigame)
 void commandSelector(string userInput)
 {
     // Handle user input for movement and other commands
@@ -300,7 +318,7 @@ void commandSelector(string userInput)
             {
                 userPosition.X += -1;
                 turnsLeft--;
-                Log.Add(userPosition); // Add the new position to the log
+                Log.Add("left"); // Add the new command to the log
             }
             else
             {
@@ -316,7 +334,7 @@ void commandSelector(string userInput)
             {
                 userPosition.X += 1;
                 turnsLeft--;
-                Log.Add(userPosition);
+                Log.Add("right"); // Add the inputed command to the log
             }
             else
             {
@@ -332,7 +350,7 @@ void commandSelector(string userInput)
             {
                 userPosition.Y -= 1;
                 turnsLeft--;
-                Log.Add(userPosition);
+                Log.Add("up"); // Add the inputed command to the log
             }
             else
             {
@@ -348,7 +366,7 @@ void commandSelector(string userInput)
             {
                 userPosition.Y += 1;
                 turnsLeft--;
-                Log.Add(userPosition);
+                Log.Add("down"); // Add the inputed command to the log
             }
             else
             {
@@ -360,6 +378,8 @@ void commandSelector(string userInput)
             break;
 
         case "hint":
+            Log.Add("hint"); // Add the inputed command to the log
+
             // IF the user has NOT found the goal point them in the right direction.
             if (!userFoundGoal)
             {
@@ -404,71 +424,66 @@ void commandSelector(string userInput)
             break;
 
         case "help":
-            Console.WriteLine("\nPossible paths: 'Left', 'Right', 'Up', 'Down'");
+            Log.Add("help"); // Add the inputed command to the log
+
+            // Display the help menu to the user
+            Console.WriteLine("\nPossible paths: 'Left', 'Right', 'Up' & 'Down'");
             Console.WriteLine("(Attempts to move in the specified direction)\t\t\t(Moving into a room consumes a turn)");
             Console.WriteLine("(not writing anything ('') will keep you in the same room)\t(Remaining in the same room does NOT consume a turn)\n");
 
-            Console.WriteLine("Other commands: 'Hint', 'ShowMap', 'Quit'\n(These commands dont consume a turn)\n");
+            Console.WriteLine("Other commands: 'Hint', 'Show Map', 'Log', 'Redo' & 'Quit'\t(These commands dont consume a turn)\n");
 
-            Console.WriteLine("'Hint': points you in the direction of the goal");
-            Console.WriteLine("'ShowMap': Displays the map of the mansion. (Also shows special rooms on the map, IF you've found them)");
-            Console.WriteLine("'Quit': (Obviously) quits the program");
+            Console.WriteLine("'Hint': points you in the direction of the goal.\t\t(Prioritizing X-axis over Y)");
+            Console.WriteLine("'Show Map': Displays the map of the mansion.\t\t\t(Also shows special rooms on the map, IF you've found them)");
+            Console.WriteLine("'Log': Displays a log of all the commands you've inputed so far");
+            Console.WriteLine("'Redo': Takes you back to the state prior to the last command");
+            Console.WriteLine("'Quit': Quits the program");
 
+            // Effectively just a pause so the user can read the help menu before continuing
             Console.WriteLine("\nPress 'Enter' to continue");
             Console.ReadLine();
             break;
 
         case "showmap":
+            Log.Add("showmap"); // Add the inputed command to the log
+
             displayMap(userPosition);
             Console.WriteLine("Press 'Enter' to continue");
             Console.ReadLine();
             break;
         case "show map":
+            Log.Add("show map"); // Add the inputed command to the log
+
             displayMap(userPosition);
             Console.WriteLine("Press 'Enter' to continue");
             Console.ReadLine();
             break;
 
         case "log":
+            Log.Add("log"); // Add the inputed command to the log
 
-            if (Log.Count > 1)
+            Console.WriteLine("Your log is as follows:");
+            int nrOfcmds = 0;
+            foreach (string? item in Log)
             {
-                Console.WriteLine("Your movement log (in order of your moves) is as follows:");
-                int nrOfMoves = -1;
-                foreach (var item in Log)
-                {
-                    Console.Write($"{++nrOfMoves}. [{item.X} {item.Y}], ");
-                }
-                Console.WriteLine(""); // Just to add a new (empty) line after the log is printed
-
-                Console.WriteLine("\nPress 'Enter' to continue");
-                Console.ReadLine();
-            } 
-            else
-            {
-                Console.WriteLine("You haven't made any moves yet!"); 
-                Console.WriteLine("\nPress 'Enter' to continue");
-                Console.ReadLine();
+                Console.Write($"{++nrOfcmds}. [{item}], ");
             }
+            Console.WriteLine(""); // Just to add a new (empty) line after the log is printed
+
+            Console.WriteLine("\nPress 'Enter' to continue");
+            Console.ReadLine();
             break;
 
         case "redo":
-            if (Log.Count > 1)
-            {
-                Console.WriteLine("Redoing your last move...");
-                userPosition = new Vector2(Log[Log.Count - 2].Y, Log[Log.Count - 2].X); // Move the user to the second last position in the log
-                Log.RemoveAt(Log.Count - 1); // Remove the last position from the log
-                turnsLeft--;
-            }
-            else
-            {
-                Console.WriteLine("You haven't made any moves yet!");
-                Console.WriteLine("\nPress 'Enter' to continue");
-                Console.ReadLine();
-            }
+            Log.Add("redo"); // Add the inputed command to the log
+
+            // Redo the last command in the log, if there is one
+            Console.WriteLine("not implimented yet.");
+
             break;
 
         case "quit":
+            Log.Add("quit"); // Add the inputed command to the log
             isPlaying = false;
             break;
 
@@ -496,67 +511,67 @@ pickRPS:
         userInput = Console.ReadLine().Trim().ToLower();
     }
 
+    Log.Add($"{userInput}"); // Add the inputed command to the log
+
     Console.WriteLine($"\nAs you pick up the {userInput} the two other items on the table vanish and a strange voice says:");
     Console.Write($"'Your choice is made~...'\n\nThe voice continues:\n'Now I will choose~...'");
 
-    int opponentChoiceNumber = new Random().Next(1, 4); // randomly choose a number between 1 and 3 for the opponent's choice
+    int opponentChoiceNumber = new Random().Next(0, 3); // randomly choose a number between 0 and 2 for the opponent's choice
     string opponentChoiceTxt = "";
 
     // Display the opponent's choice
     switch (opponentChoiceNumber)
     {
-        case 1:
+        case 0:
             Console.WriteLine("Rock!\n");
             opponentChoiceTxt = "Rock";
             break;
-        case 2:
+        case 1:
             Console.WriteLine("Paper!\n");
             opponentChoiceTxt = "Paper";
             break;
-        case 3:
+        case 2:
             Console.WriteLine("Scissors!\n");
             opponentChoiceTxt = "Scissors";
             break;
     }
 
-    // Now to determine the winner of the Rock Paper Scissors game
-
     // Prepare a variable to hold the user's choice as a number
-    int userRPSchoice = 0;
-    // Convert the user's choice to a number for comparison
-    switch (userInput)
+    int userRPSchoice = 0; // This is just a temporary value and will be set based on the user's input below
+    switch (userInput) // Convert the user's choice to a number for comparison
     {
         case "rock":
-            userRPSchoice = 1;
+            userRPSchoice = 0;
             break;
         case "paper":
-            userRPSchoice = 2;
+            userRPSchoice = 1;
             break;
         case "scissors":
-            userRPSchoice = 3;
+            userRPSchoice = 2;
             break;
     }
 
-    // Determine the result of the game by the following truth table:
+    // Now to determine the winner of the Rock Paper Scissors game,
+    // by the following truth table:
     /*
      | ----------- | -------- | ---------- | ----------- |
-     | user →      | rock (1) | papper (2) | scissor (3) |
+     | user →      | rock (0) | papper (1) | scissor (2) |
      | Opp ↓       |          |            |             |
      | ----------- | -------- | ---------- | ----------- |
-     | rock (1)    |    0     |     1      |     2       |
+     | rock (0)    |    0     |     1      |     2       |
      | ----------- | -------- | ---------- | ----------- |
-     | papper (2)  |    -1    |     0      |     1       |
+     | papper (1)  |    2     |     0      |     1       |
      | ----------- | -------- | ---------- | ----------- |
-     | scissor (3) |    -2    |     -1     |     0       |
+     | scissor (2) |    1     |     2      |     0       |
      | ----------- | -------- | ---------- | ----------- |
     */
-    int result = userRPSchoice - opponentChoiceNumber;
+    int result = (userRPSchoice - opponentChoiceNumber + 3) % 3;
     // switch statement to handle the result of the RPS game
     switch (result)
     {
         // It's a tie
         case 0:
-            Console.WriteLine("The voice continues:\n'You have chosen the same as I, so we are equal~'");
+            Console.WriteLine("After a pause. The voice continues:\n'You have chosen the same as I, so we are equal~'");
             Console.WriteLine("\nPress 'Enter' to continue");
             Console.ReadLine();
             Console.Clear();
@@ -564,14 +579,15 @@ pickRPS:
 
             Console.WriteLine("Which do you choose?\n[rock], [paper], or [scissors]:");
             userInput = Console.ReadLine().Trim().ToLower();
-            goto pickRPS;
+
+            goto pickRPS; // Go the the label and effectively restart the RPS game, until a winner is determined (Best of 1)
 
         // The user wins
         case 1:
-            Console.WriteLine($"The voice continues:\n'You have chosen better than I. {userInput} beats {opponentChoiceTxt} therefore you win!'");
-            if (userFoundGoal)
+            Console.WriteLine($"After a pause. The voice continues:\n'You have chosen better than I. {userInput} beats {opponentChoiceTxt} therefore you win!'");
+            if (userFoundGoal == false)
             {
-                Console.WriteLine("'As a reward I'll reveal where the goal of your exploration is located!'");
+                Console.WriteLine("'\nAs a reward I'll reveal where the goal of your exploration is located!'");
                 userFoundGoal = true;
                 displayMap(userPosition);
                 Console.WriteLine("\nPress 'Enter' to continue");
@@ -580,29 +596,7 @@ pickRPS:
             }
             else
             {
-                Console.WriteLine("'It seems you've already found the goal....\nSo! I'll simply give you more time to explore as you like!'");
-                Console.WriteLine("The mansion stabilizes slightly...");
-                turnsLeft += 5;
-                Console.WriteLine($"(You now have {turnsLeft} turns left)");
-                Console.WriteLine("\nPress 'Enter' to continue");
-                Console.ReadLine();
-                Console.Clear();
-            }
-            break;
-        case 2:
-            Console.WriteLine($"The voice continues:\n'You have chosen better than I. {userInput} beats {opponentChoiceTxt} therefore you win!'");
-            if (userFoundGoal)
-            {
-                Console.WriteLine("'As a reward I'll reveal where the goal of your exploration is located!'");
-                userFoundGoal = true;
-                displayMap(userPosition);
-                Console.WriteLine("\nPress 'Enter' to continue");
-                Console.ReadLine();
-                Console.Clear();
-            }
-            else
-            {
-                Console.WriteLine("'It seems you've already found the goal....\nSo! I'll simply give you more time to explore as you like!'");
+                Console.WriteLine("'\nIt seems you've already found the goal....\nSo! I'll simply give you more time to explore as you like!'");
                 Console.WriteLine("The mansion stabilizes slightly...");
                 turnsLeft += 5;
                 Console.WriteLine($"(You now have {turnsLeft} turns left)");
@@ -612,20 +606,14 @@ pickRPS:
             }
             break;
 
-        // Opponent wins
-        case -1:
-            Console.WriteLine($"The voice continues:\n'You have chosen worse than I. {opponentChoiceTxt} beats {userInput} therefore you lose.'");
-            Console.WriteLine("'As a punishment I'll teleport you somewhere in the mansion!'");
-            userPosition = new Vector2(new Random().Next(0, grid.GetLength(0)), new Random().Next(0, grid.GetLength(1)));
-            displayMap(userPosition);
-            Console.WriteLine("\nPress 'Enter' to continue");
-            Console.ReadLine();
-            Console.Clear();
-            break;
-        case -2:
-            Console.WriteLine($"The voice continues:\n'You have chosen worse than I. {opponentChoiceTxt} beats {userInput} therefore you lose.'");
-            Console.WriteLine("'As a punishment I'll teleport you somewhere in the mansion!'");
-            userPosition = new Vector2(new Random().Next(0, grid.GetLength(0)), new Random().Next(0, grid.GetLength(1)));
+        // The opponent wins
+        case 2:
+            Console.WriteLine($"After a pause. The voice continues:\n'You have chosen worse than I. {opponentChoiceTxt} beats {userInput} therefore you lose.'");
+            Console.WriteLine("'As a punishment I'll teleport you somewhere else in the mansion!'");
+            while (userPosition != RPS_Position)
+            {
+                userPosition = new Vector2(new Random().Next(0, grid.GetLength(0)), new Random().Next(0, grid.GetLength(1)));
+            }
             displayMap(userPosition);
             Console.WriteLine("\nPress 'Enter' to continue");
             Console.ReadLine();
